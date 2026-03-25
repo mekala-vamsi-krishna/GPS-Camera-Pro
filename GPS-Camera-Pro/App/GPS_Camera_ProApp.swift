7be05b525cbaebf65aa4350345955b4aa19e7abf
kundancodelab <kundaniosdev@gmail.com>
2026-03-25 22:54:34 +0530

    set up  project

---
+++ GPS-Camera-Pro/App/GPS_Camera_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPS_Camera_ProApp.swift
+//  GPS-Camera-Pro
+//
+//  Created by User on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GPS_Camera_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GPS-Camera-Pro/Utilites/Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Utils.swift
+//  GPS-Camera-Pro
+//
+//  Created by User on 25/03/26.
+//
+
+import Foundation
